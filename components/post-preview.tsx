b7d59adb2a4a2b67e98ef887c4e0568cee038390
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2020-11-10 09:47:44 +0100

    clean up

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -22,22 +22,22 @@ const PostPreview = ({
 }: PostPreviewProps) => {
   return (
     <div>
-      <div className="mb-5">
+      <div>
         <CoverImage
           slug={slug}
           title={title}
           responsiveImage={coverImage.responsiveImage}
         />
       </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      <h3>
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a className="hover:underline">{title}</a>
+          <a>{title}</a>
         </Link>
       </h3>
-      <div className="text-lg mb-4">
+      <div>
         <Date dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+      <p>{excerpt}</p>
     </div>
   )
 }
