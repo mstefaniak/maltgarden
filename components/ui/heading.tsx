de53c7f89fa63fcf67672004aac6ea9b813bd2e8
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2021-03-20 12:59:42 +0100

    optimize heading

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import useTranslation from 'next-translate/useTranslation'
 import styles from './heading.module.scss'
 
@@ -5,11 +6,21 @@ const Heading = () => {
   const { t } = useTranslation()
 
   return (
-    <section className={styles.slogan}>
-      <h1>
-        <span>{t('common:home_slogan1')}</span>
-        <span>{t('common:home_slogan2')}</span>
-      </h1>
+    <section className={styles.container}>
+      <Image
+        className={styles.bgImage}
+        src="/images/heading.png"
+        alt={`${t('common:home_slogan1')} ${t('common:home_slogan2')}`}
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+      />
+      <div className={styles.content}>
+        <h1>
+          <span>{t('common:home_slogan1')}</span>
+          <span>{t('common:home_slogan2')}</span>
+        </h1>
+      </div>
     </section>
   )
 }
