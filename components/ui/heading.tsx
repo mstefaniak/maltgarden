cbc858963b632c28c731fd7373cb370f944328d4
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2021-03-20 13:11:38 +0100

    animate heading on load

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import useTranslation from 'next-translate/useTranslation'
 import styles from './heading.module.scss'
 
 const Heading = () => {
+  const [isReady, setIsReady] = useState(false)
   const { t } = useTranslation()
 
   return (
     <section className={styles.container}>
       <Image
-        className={styles.bgImage}
+        className={`
+          ${styles.bgImage}
+          ${isReady ? styles.ready : ''}
+        `}
         src="/images/heading.png"
         alt={`${t('common:home_slogan1')} ${t('common:home_slogan2')}`}
         layout="fill"
         objectFit="cover"
         objectPosition="center"
+        onLoad={() => setIsReady(true)}
       />
       <div className={styles.content}>
         <h1>
