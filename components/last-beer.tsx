5e5a5072ba53c78ac3cae002610ae6d88687e23a
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2021-04-09 15:05:51 +0200

    minor updates

--- components/last-beer.tsx
+++ components/last-beer.tsx
@@ -1,5 +1,5 @@
 import { Slide } from 'react-awesome-reveal'
-import Image from 'next/image'
+import { Image } from 'react-datocms'
 import useTranslation from 'next-translate/useTranslation'
 import { Beer } from '@/lib/types'
 import styles from './last-beer.module.scss'
@@ -10,26 +10,16 @@ interface ILastBeerProps {
 
 const LastBeer = ({ beer }: ILastBeerProps) => {
   const { t } = useTranslation()
-  const img = beer.photoWithBackground.responsiveImage
-  const bg = beer.backgroundImage.responsiveImage
 
   return (
     <section className={styles.lastBeer}>
       <div className={styles.background}>
-        <Image
-          src={bg.src as string}
-          alt={bg.alt}
-          title={bg.title}
-          layout="fill"
-        />
+        <Image data={beer.backgroundImage.responsiveImage} />
       </div>
       <Slide triggerOnce={true} direction="left" className={styles.image}>
         <Image
-          src={img.src as string}
-          alt={img.alt}
-          title={img.title}
-          width={img.width}
-          height={img.height as number}
+          data={beer.photoWithBackground.responsiveImage}
+          lazyLoad={false}
         />
       </Slide>
       <Slide
