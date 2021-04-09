55d00fed6f951626b60f41c9b4a7a85d0792e61f
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2021-04-09 18:58:09 +0200

    clean up deprecated _pages

--- _pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app'
-import '@/styles/global.scss'
-
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
-
-export default App
