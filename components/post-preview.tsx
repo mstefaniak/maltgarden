8583edb1f5016854ef45e38ad9c3ce78b9199007
Marcin Stefaniak <marcin.stefaniak@auto1.com>
2021-05-10 06:41:33 +0200

    remove unused dependencies

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,8 +1,3 @@
-import { Date } from './date'
-import { CoverImage } from './cover-image'
-import Link from 'next/link'
-import { RoundLink } from '@/components/ui/round-link'
-import useTranslation from 'next-translate/useTranslation'
 import { Post } from '@/lib/types'
 import { PostBody } from '@/components/post-body'
 import styles from './post-preview.module.scss'
