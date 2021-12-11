d028c35051a746cd410004b9d001251ceed14b7f
mstefaniak <marcin.stefaniak@plentific.com>
2021-12-11 14:40:19 +0100

    simplify age verification component

--- components/age.tsx
+++ components/age.tsx
@@ -8,13 +8,12 @@ import cn from 'classnames'
 import styles from './age.module.scss'
 
 interface AgeProps {
-  isOpen: boolean
   onVerify: () => void
 }
 
 const LOADING_TIME = 1000
 
-const Age = ({ onVerify, isOpen = true }: AgeProps) => {
+const Age = ({ onVerify }: AgeProps) => {
   const [isLoaded, setIsLoaded] = useState(false)
   const { t } = useTranslation()
 
@@ -26,62 +25,57 @@ const Age = ({ onVerify, isOpen = true }: AgeProps) => {
 
   return (
     <div
-      className={cn(styles.container, {
-        [styles.isOpen]: isOpen,
+      className={cn(styles.container, styles.isOpen, {
         [styles.isLoaded]: isLoaded,
       })}
     >
-      {isOpen && (
-        <>
-          <div className={styles.bgImage}>
-            <Image
-              src="/images/age_check_bg.png"
-              alt="Colorful waves in the background"
-              layout="fill"
-            />
-          </div>
-          <div className={styles.content}>
-            {isMobile && (
-              <Slide delay={1250} direction="down">
-                <div className={styles.logoImage}>
-                  <Image
-                    src="/images/logo.svg"
-                    alt="Maltgarden Brewery Logo"
-                    width={145}
-                    height={105}
-                  />
-                </div>
-              </Slide>
-            )}
-            <p>{t('home:ageInfo1')}</p>
-            <h1 className={styles.heading}>{t('home:ageInfo2')}</h1>
-            <div className={styles.actions}>
-              <Slide delay={1000} direction="left">
-                <RoundLink onClick={onVerify} href="" text={t('home:yes')} />
-              </Slide>
-              <Slide delay={1000} direction="right">
-                <RoundLink
-                  href="http://google.com"
-                  rel="noindex nofollow"
-                  text={t('home:no')}
-                />
-              </Slide>
+      <div className={styles.bgImage}>
+        <Image
+          src="/images/age_check_bg.png"
+          alt="Colorful waves in the background"
+          layout="fill"
+        />
+      </div>
+      <div className={styles.content}>
+        {isMobile && (
+          <Slide delay={1250} direction="down">
+            <div className={styles.logoImage}>
+              <Image
+                src="/images/logo.svg"
+                alt="Maltgarden Brewery Logo"
+                width={145}
+                height={105}
+              />
             </div>
-            {!isMobile && (
-              <Slide delay={1250} direction="up">
-                <div className={styles.logoImage}>
-                  <Image
-                    src="/images/logo.svg"
-                    alt="Maltgarden Brewery Logo"
-                    width={285}
-                    height={205}
-                  />
-                </div>
-              </Slide>
-            )}
-          </div>
-        </>
-      )}
+          </Slide>
+        )}
+        <p>{t('home:ageInfo1')}</p>
+        <h1 className={styles.heading}>{t('home:ageInfo2')}</h1>
+        <div className={styles.actions}>
+          <Slide delay={1000} direction="left">
+            <RoundLink onClick={onVerify} href="" text={t('home:yes')} />
+          </Slide>
+          <Slide delay={1000} direction="right">
+            <RoundLink
+              href="http://google.com"
+              rel="noindex nofollow"
+              text={t('home:no')}
+            />
+          </Slide>
+        </div>
+        {!isMobile && (
+          <Slide delay={1250} direction="up">
+            <div className={styles.logoImage}>
+              <Image
+                src="/images/logo.svg"
+                alt="Maltgarden Brewery Logo"
+                width={285}
+                height={205}
+              />
+            </div>
+          </Slide>
+        )}
+      </div>
     </div>
   )
 }
